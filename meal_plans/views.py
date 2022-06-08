4546039b38b03e19eb6c9384ca52f9beed3189f1
Alex Porter <alpo1992@gmail.com.here>
2022-06-08 15:51:06 -0600

    .

--- meal_plans/views.py
+++ meal_plans/views.py
@@ -4,7 +4,6 @@ from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
-from django.core.paginator import Paginator
 
 
 from meal_plans.models import MealPlan
@@ -19,7 +18,7 @@ class MealPlanListView(ListView):
         return context
 
 
-class RecipeCreateView(LoginRequiredMixin, CreateView):
+class MealPlanCreateView(LoginRequiredMixin, CreateView):
     model = MealPlan
     template_name = "meal_plan/new.html"
     fields = ["name", "date", "recipes"]
@@ -30,10 +29,23 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class DetailView(LoginRequiredMixin, DetailView):
+class MealPlanDetailView(LoginRequiredMixin, DetailView):
     model = MealPlan
     template_name = "meal_plan/detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class MealPlanUpdateView(LoginRequiredMixin, UpdateView):
+    model = MealPlan
+    template_name = "meal_plan/edit.html"
+    fields = ["name", "date", "recipes"]
+    success_url = reverse_lazy("meal_plan_list")
+
+
+class MealPlanDeleteView(LoginRequiredMixin, DeleteView):
+    model = MealPlan
+    template_name = "meal_plan/delete.html"
+    success_url = reverse_lazy("meal_plan_list")

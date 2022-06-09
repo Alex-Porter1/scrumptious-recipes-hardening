0db65d8412d54917c038e748471f3f9e40c867a4
Alex Porter <alpo1992@gmail.com.here>
2022-06-08 19:11:13 -0600

    .

--- meal_plans/models.py
+++ meal_plans/models.py
@@ -10,9 +10,12 @@ class MealPlan(models.Model):
     name = models.CharField(max_length=120)
     date = models.DateField()
     owner = models.ForeignKey(
-        USER_MODEL, related_name="meal_plans", on_delete=models.CASCADE, null=True
+        USER_MODEL,
+        related_name="meal_plans",
+        on_delete=models.CASCADE,
+        null=True,
     )
     recipes = models.ManyToManyField("recipes.Recipe", related_name="recipes")
 
     def __str__(self):
-        return self.name
+        return self.name + " by " + str(self.owner)
--- meal_plans/views.py
+++ meal_plans/views.py
@@ -13,7 +13,7 @@ class MealPlanListView(ListView):
     model = MealPlan
     template_name = "meal_plans/list.html"
     paginate_by = 2
-    context_object_name = "meals_plans_list"
+    context_object_name = "meal_plans_list"
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
     #     return context
@@ -23,8 +23,16 @@ class MealPlanCreateView(LoginRequiredMixin, CreateView):
     model = MealPlan
     template_name = "meal_plans/new.html"
     fields = ["name", "date", "recipes"]
-    success_url = reverse_lazy("meal_plan_detail")
+    
+    def form_valid(self, form):
+        plan = form.save(commit=False)
+        plan.owner = self.request.user
+        plan.save()
+        form.save_m2m()
+        return redirect("meal_plan_detail", pk=plan.id)
 
+    def get_queryset(self):
+        return MealPlan.objects.filter(owner=self.request.user)
     # def form_valid(self, form):
     #     form.instance.owner = self.request.user
     #     return super().form_valid(form)
@@ -34,19 +42,29 @@ class MealPlanDetailView(LoginRequiredMixin, DetailView):
     model = MealPlan
     template_name = "meal_plans/detail.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     return context
 
 
 class MealPlanUpdateView(LoginRequiredMixin, UpdateView):
     model = MealPlan
     template_name = "meal_plans/edit.html"
     fields = ["name", "date", "recipes"]
-    success_url = reverse_lazy("meal_plans_list")
+    # success_url = reverse_lazy("meal_plans_list")
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("meal_plan_detail", args=[self.object.id])
 
 
 class MealPlanDeleteView(LoginRequiredMixin, DeleteView):
     model = MealPlan
     template_name = "meal_plans/delete.html"
     success_url = reverse_lazy("meal_plans_list")
+
+    def form_valid(self, form):
+        plan = form.save(commit=False)
+        plan.owner = self.request.user
+        plan.save()
+        form.save_m2m()
+        return redirect("meal_plans_list", pk=plan.id)
